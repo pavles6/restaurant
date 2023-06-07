3cfef5f4c9b8be377be97ffd04d849c373d76986
pavles6 <pavles6@protonmail.com>
2023-06-07 18:04:31 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+mod front_of_house;
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+}
